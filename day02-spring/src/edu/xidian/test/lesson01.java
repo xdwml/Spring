6c52a684f27b7c5b17ee0a5e824725c80a577ed6
wml <18270856781@163.com>
2020-02-09 17:54:15 +0800

    Signed-off-by: wml <18270856781@163.com>

--- day02-spring/src/edu/xidian/test/lesson01.java
+++ day02-spring/src/edu/xidian/test/lesson01.java
@@ -2,14 +2,8 @@ package edu.xidian.test;
 
 import edu.xidian.service.UserService;
 import org.junit.Test;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-import org.springframework.core.ResolvableType;
 import org.springframework.core.io.FileSystemResource;
 
 /**
